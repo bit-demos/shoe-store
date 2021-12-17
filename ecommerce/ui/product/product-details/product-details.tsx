c1d32932a3937435b93676a2d103c0267aabde0b
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-12-17 16:27:59 +0100

    store working :)

--- ecommerce/ui/product/product-details/product-details.tsx
+++ ecommerce/ui/product/product-details/product-details.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useReducer, useEffect } from 'react';
 import classNames from 'classnames';
 import { Img } from '@learn-bit-react/base-ui.ui.img';
 import { Heading } from '@learn-bit-react/base-ui.ui.heading';
@@ -10,6 +10,7 @@ import { Counter } from '@learn-bit-react/ecommerce.ui.product.counter';
 import { Button } from '@learn-bit-react/base-ui.ui.button';
 import { SelectSize } from '@learn-bit-react/ecommerce.ui.product.select-size';
 import { Shoe, Shoes } from '@learn-bit-react/shoe-store.entity.shoes';
+import { allShoes } from '@learn-bit-react/shoe-store.entity.shoes';
 
 import styles from './product-details.module.scss';
 
@@ -20,9 +21,126 @@ export type ProductDetailsProps = {
   product: Shoes;
 } & React.HTMLAttributes<HTMLDivElement>;
 
+export const ADD_PRODUCT = 'ADD_PRODUCT';
+export const REMOVE_PRODUCT = 'REMOVE_PRODUCT';
+
+const MainNavigation = (props) => (
+  <header className="main-navigation">Cart ({props.cartItemNumber})</header>
+);
+const addProductToCart = (
+  product,
+  selectedSize,
+  selectedColor,
+  selectedQuantity,
+  state
+) => {
+  const updatedCart = [...state.cart];
+
+  updatedCart.push({
+    ...product,
+    quantity: selectedQuantity,
+    selectedSize,
+    selectedColor
+  });
+
+  return { ...state, cart: updatedCart };
+};
+
+const removeProductFromCart = (productId, state) => {
+  console.log('Removing product with id: ' + productId);
+  const updatedCart = [...state.cart];
+  const updatedItemIndex = updatedCart.findIndex(
+    (item) => item.product.id === productId
+  );
+
+  const updatedItem = {
+    ...updatedCart[updatedItemIndex]
+  };
+  updatedItem.quantity--;
+  if (updatedItem.quantity <= 0) {
+    updatedCart.splice(updatedItemIndex, 1);
+  } else {
+    updatedCart[updatedItemIndex] = updatedItem;
+  }
+  return { ...state, cart: updatedCart };
+};
+
+export const shopReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return addProductToCart(
+        action.product,
+        action.selectedSize,
+        action.selectedColor,
+        action.selectedQuantity,
+        state
+      );
+    case REMOVE_PRODUCT:
+      return removeProductFromCart(action.productId, state);
+    default:
+      return state;
+  }
+};
+const ShopContext = React.createContext({
+  products: allShoes,
+  cart: [],
+  addProductToCart: (
+    product,
+    selectedSize,
+    selectedColor,
+    selectedQuantity
+  ) => {},
+  removeProductFromCart: (productId) => {}
+});
+
+const GlobalState = (props) => {
+  const products = allShoes;
+  const [cartState, dispatch] = useReducer(shopReducer, { cart: [] });
+
+  const addProductToCart = (
+    product,
+    selectedSize,
+    selectedColor,
+    selectedQuantity
+  ) => {
+    setTimeout(() => {
+      dispatch({
+        type: ADD_PRODUCT,
+        product,
+        selectedSize,
+        selectedColor,
+        selectedQuantity
+      });
+    }, 100);
+  };
+
+  const removeProductFromCart = (productId) => {
+    setTimeout(() => {
+      dispatch({ type: REMOVE_PRODUCT, productId: productId });
+    }, 100);
+  };
+
+  return (
+    <ShopContext.Provider
+      value={{
+        products: products,
+        cart: cartState.cart,
+        addProductToCart: addProductToCart,
+        removeProductFromCart: removeProductFromCart
+      }}
+    >
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
 export function ProductDetails({ product }: ProductDetailsProps) {
-  const [addedToCart, setAddedToCart] = useState(false);
-  const [shoe, setShoe] = useState<Shoe>({} as Shoe);
+  const context = useContext(ShopContext);
+  const [selectedSize, setSelectedSize] = useState(product.availableSizes[0]);
+  const [selectedColor, setSelectedColor] = useState(
+    product.availableColors[0]
+  );
+  const [selectedQuantity, setSelectedQuantity] = useState(1);
 
   const {
     product: { id, src, title, text, price, alt, rating },
@@ -30,82 +148,117 @@ export function ProductDetails({ product }: ProductDetailsProps) {
     availableSizes
   } = product;
 
-  function handleClick(id) {
-    setAddedToCart(true);
-    console.log(shoe);
-    // context.addToCart(shoe); // comes from context
-  }
-
   function sizeChanged(size) {
-    setShoe({ ...shoe, size: size });
+    setSelectedSize(parseInt(size));
   }
 
   function colorChanged(color) {
-    setShoe({ ...shoe, color: color });
+    setSelectedColor(color);
   }
 
   function quantityChanged(quantity) {
-    setShoe({ ...shoe, quantity: quantity });
+    setSelectedQuantity(quantity);
   }
 
   return (
-    <div
-      className={classNames(
-        styles.productDetails,
-        'flex flex-col pt-6 md:flex-row '
-      )}
-    >
-      <Img
-        className={classNames(styles.img, 'mx-auto md:mx-0')}
-        src={src}
-        alt={alt}
-      />
-      <div className={classNames(styles.details, 'xs:p-16 md:pl-16')}>
-        <Heading element="h2" className="text-5xl mb-8">
-          {title}
-        </Heading>
-        <Rating stars={rating} className="mb-4" />
-        <Currency price={price} className="mb-4 text-2xl" />
-        <Text className="mb-4">{text}</Text>
-        <div>
-          <Counter quantitySelected={quantityChanged} className="my-4" />
-          <div className="my-4">
-            <label htmlFor="size">Choose a size: </label>
-            <SelectSize
-              id="size"
-              sizeSelected={sizeChanged}
-              className={styles.select}
-              availableSizes={availableSizes}
+    <GlobalState>
+      <ShopContext.Consumer>
+        {(context) => (
+          <React.Fragment>
+            <MainNavigation
+              cartItemNumber={context.cart.reduce((count, curItem) => {
+                return count + curItem.quantity;
+              }, 0)}
             />
-          </div>
-        </div>
-        <Text>Choose a color: </Text>
-        <AvailableColors
-          colorSelected={colorChanged}
-          className={styles.availableColors}
-          availableColors={availableColors}
-        />
-        <Button
-          primary
-          onClick={() => {
-            handleClick(id);
-          }}
-        >
-          Add to Cart
-        </Button>
-        {/* {addedToCart ? <Text>shoe size: {shoe.size}</Text> : null}
+            {context.cart.reduce((count, curItem) => {
+              return count + curItem.quantity;
+            }, 0)}
+            <div className="flex flex-row">
+              <div
+                className={classNames(
+                  styles.productDetails,
+                  'flex flex-col pt-6 md:flex-row '
+                )}
+              >
+                <Img
+                  className={classNames(styles.img, 'mx-auto md:mx-0')}
+                  src={src}
+                  alt={alt}
+                />
+                <div className={classNames(styles.details, 'xs:p-16 md:pl-16')}>
+                  <Heading element="h2" className="text-5xl mb-8">
+                    {title}
+                  </Heading>
+                  <Rating stars={rating} className="mb-4" />
+                  <Currency price={price} className="mb-4 text-2xl" />
+                  <Text className="mb-4">{text}</Text>
+                  <div>
+                    <Counter
+                      quantitySelected={quantityChanged}
+                      className="my-4"
+                    />
+                    <div className="my-4">
+                      <label htmlFor="size">Choose a size: </label>
+                      <SelectSize
+                        id="size"
+                        sizeSelected={sizeChanged}
+                        className={styles.select}
+                        availableSizes={availableSizes}
+                      />
+                    </div>
+                  </div>
+                  <Text>Choose a color: </Text>
+                  <AvailableColors
+                    colorSelected={colorChanged}
+                    className={styles.availableColors}
+                    availableColors={availableColors}
+                  />
+                  <Button
+                    primary
+                    onClick={() =>
+                      context.addProductToCart(
+                        product,
+                        selectedSize,
+                        selectedColor,
+                        selectedQuantity
+                      )
+                    }
+                  >
+                    Add to Cart
+                  </Button>
+                </div>
+              </div>
 
-        {addedToCart ? <Text>color: {shoe.color}</Text> : null}
-
-        {addedToCart ? (
-          <Text>
-            quantity:
-            {shoe.quantity}
-          </Text>
-        ) : null} */}
-
-        {addedToCart ? <Text>Added to cart {title} Shoes</Text> : null}
-      </div>
-    </div>
+              <main className="cart">
+                {context.cart.length <= 0 && <p>No Item in the Cart!</p>}
+                <ul>
+                  {context.cart.map((cartItem, index) => (
+                    <li key={cartItem.product.id + index}>
+                      <div>
+                        <p>size: selected{cartItem.selectedSize}</p>
+                        <p>color: selected{cartItem.selectedColor}</p>
+                        <p>color: selected{cartItem.quantity}</p>
+                        <strong>{cartItem.product.title}</strong> -{' '}
+                        {cartItem.product.price}
+                      </div>
+                      <div>
+                        <Button
+                          secondary
+                          onClick={() =>
+                            context.removeProductFromCart(cartItem.product.id)
+                          }
+                        >
+                          Remove from Cart
+                        </Button>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </main>
+            </div>
+          </React.Fragment>
+        )}
+      </ShopContext.Consumer>
+    </GlobalState>
   );
 }
