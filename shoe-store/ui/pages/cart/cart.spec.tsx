6f2501454378f9531f70b9cfbfbaf6a756467877
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2022-02-15 20:48:57 +0100

    fix tests

--- shoe-store/ui/pages/cart/cart.spec.tsx
+++ shoe-store/ui/pages/cart/cart.spec.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BasicCart } from './cart.composition';
+import { EmptyCart } from './cart.composition';
 
 it.skip('should render with the correct text', () => {
-  const { getByText } = render(<BasicCart />);
-  const rendered = getByText('hello from Cart');
+  const { getByText } = render(<EmptyCart />);
+  const rendered = getByText('no products to display');
   expect(rendered).toBeTruthy();
 });
