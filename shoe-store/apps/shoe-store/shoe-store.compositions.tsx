5f652317a7a900caf6da03d18c526669596724ed
bengil <benjyg@gmail.com>
2022-03-14 14:03:40 +0200

    add default theme composition for store

--- shoe-store/apps/shoe-store/shoe-store.compositions.tsx
+++ shoe-store/apps/shoe-store/shoe-store.compositions.tsx
@@ -4,6 +4,17 @@ import { ReactRouterRoutingProvider } from '@teambit/ui-foundation.ui.navigation
 import { ShoeStoreApp } from './app';
 import { pinkTheme } from '@learn-bit-react/base-ui.themes.pink-theme';
 import { purpleTheme } from '@learn-bit-react/base-ui.themes.purple-theme';
+
+export const ShoeStoreDefaultTheme = () => {
+  return (
+    <MemoryRouter>
+      <ReactRouterRoutingProvider>
+        <ShoeStoreApp></ShoeStoreApp>
+      </ReactRouterRoutingProvider>
+    </MemoryRouter>
+  );
+};
+
 export const ShoeStorePinkTheme = () => {
   return (
     <MemoryRouter>
